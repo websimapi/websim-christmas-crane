7029d3f25c4bfa10ea81b2650d0ae8c4513963f8
api <api@websim.ai>
2025-12-10 21:07:38 +0000

    v2: I dont see a playable gane investigate and fix

--- src/main.js
+++ src/main.js
@@ -1,2 +1,7 @@
-import './App.jsx';
+import { createRoot } from 'react-dom/client';
+import { html } from 'htm/react';
+import App from './App.js';
+
+const root = createRoot(document.getElementById('root'));
+root.render(html`<${App} />`);
 
